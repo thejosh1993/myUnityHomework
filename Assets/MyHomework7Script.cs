8bab8e7e8947b32a6201860ec6b94b7015afa58c
thejosh1993 <josh.davenport@wsu.edu>
2017-03-10 17:52:29 -0800

    Fixed
    After our tutorial in class today I was able to apply some edumakation
    and get my issues solved. Thanks Adam

--- Assets/MyHomework7Script.cs
+++ Assets/MyHomework7Script.cs
@@ -7,34 +7,35 @@ public class MyHomework7Script : MonoBehaviour
     private Renderer rend;
     public float endurance = 100;
     public float damage = 25;
-    private Vector3 position;
-
     //amount of health & damage taken per collision 
+
+    private Vector3 position;
+    public GameObject bridge;
+
 
     void Start()
     {
-        GameObject temp = GameObject.Find("bridge");
+
     }
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.name != "impact")
         {
             endurance = endurance - damage;
-            transform.localScale += new Vector3(-1, 0, -1); 
+            transform.localScale += new Vector3(-1, 0, -1);
             //decrease the size of the target
         }
-            if (endurance < 0)
-            {
-                Destroy(gameObject);
-            //upon reaching 0, target is destroyed
-
-      
-            // var MyHomework7Script_2 = transform.position += new Vector3(0, 10, 0);
+        if (endurance < 0)
+        {
+            Destroy(gameObject);
+            bridge.transform.position += new Vector3(0, 14, 0);
+            //upon reaching 0, target is destroyed, and bridge is raised.
 
 
-         
+
+
 
         }
-      }
+    }
     }
 
--- Assets/MyHomework7Script_2.cs
+++ Assets/MyHomework7Script_2.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 
 public class MyHomework7Script_2 : MonoBehaviour {
-    public object myvar;
-
+    
     // Use this for initialization
     void Start () {
-      
 
-    } 
+} 
 	
 	// Update is called once per frame
 	void Update () {
      if (Input.GetKey (KeyCode.Q))
         {
             transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Translate(0, -1, 0);
         }
     }
 }
